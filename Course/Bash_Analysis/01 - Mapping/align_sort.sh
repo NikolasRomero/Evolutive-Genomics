620f0699b28026b7b8fe0d69417b8e36996b6946
Nicolás Romero <nromerov@unal.edu.co>
2026-07-28 13:50:48 -0500

    Update align_sort.sh

--- Course/Bash_Analysis/01 - Mapping/align_sort.sh
+++ Course/Bash_Analysis/01 - Mapping/align_sort.sh
@@ -4,9 +4,9 @@ do
 	# declare variables
 	BWA_PATH=/scratchsan1/anaconda3/envs/bwa/bin/
 	REF=~/reference/GCA_917862395.2_iHelSar1.2_genomic.fna
-	FORWARD=~/filteredReads/${IND}.R1.trimmed.fastq.gz
-	REVERSE=~/filteredReads/${IND}.R2.trimmed.fastq.gz
-	OUTPUT=~/reference/align/${IND}_sort.bam
+	FORWARD=/scratchsan/C_computacion/nr10sanger_ac/biodiversity_genomics_course/data/Heliconius/WGS/filteredReads/${IND}.R1.trimmed.fastq.gz
+    REVERSE=/scratchsan/C_computacion/nr10sanger_ac/biodiversity_genomics_course/data/Heliconius/WGS/filteredReads/${IND}.R2.trimmed.fastq.gz
+    1OUTPUT=~/reference/align/${IND}_sort.bam
 
 	# read group string, required by Picard MarkDuplicates and GATK
 	RG="@RG\tID:${IND}\tSM:${IND}\tPL:ILLUMINA\tLB:${IND}"
