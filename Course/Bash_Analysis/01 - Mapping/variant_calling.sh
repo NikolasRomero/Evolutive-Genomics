f8882884beec356d1441797684ddedffbeeabda5
Nicolás Romero <nromerov@unal.edu.co>
2026-07-28 15:51:28 -0500

    Update variant_calling.sh

--- Course/Bash_Analysis/01 - Mapping/variant_calling.sh
+++ Course/Bash_Analysis/01 - Mapping/variant_calling.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+#Index again
+
+conda activate samtools
+samtools faidx GCA_917862395.2_iHelSar1.2_genomic.fna
+conda deactivate
+
+#Screen andvariable
+screen -S call_variants
+
+REF=~/reference/GCA_917862395.2_iHelSar1.2_genomic.fna
+
+## We are going to call variants within the vcf folder using bcftools:
+
+BCFTOOLS_PATH=/scratchsan/C_computacion/nr10sanger_ac/miniconda3/envs/bcftools_m/bin/
+${BCFTOOLS_PATH}/bcftools mpileup -a AD,DP,SP -Ou -f $REF \
+~/reference/align/*.sort.rmd.bam | ${BCFTOOLS_PATH}/bcftools call -f GQ,GP \
+-mO z -o ./sara_sapho.vcf.gz
 
