76c02de92938d8873dfed407e140aeb1cb83f2f4
Nicolás Romero <nromerov@unal.edu.co>
2026-07-29 09:52:01 -0500

    Create Data_Download.sh

---
+++ Course/Bash_Analysis/01 - Mapping/Data_Download.sh
@@ -0,0 +1,7 @@
+## Data Download to R
+
+scp -r -J nromerov@168.176.34.122 nromerov@perseus:/scratchsan/C_computacion/nromerov/vcf/vcftools/sara_sapho.idepth ./
+scp -r -J nromerov@168.176.34.122 nromerov@perseus:/scratchsan/C_computacion/nromerov/vcf/vcftools/sara_sapho.imiss ./
+scp -r -J nromerov@168.176.34.122 nromerov@perseus:/scratchsan/C_computacion/nromerov/vcf/vcftools/sara_sapho.ldepth.mean ./
+scp -r -J nromerov@168.176.34.122 nromerov@perseus:/scratchsan/C_computacion/nromerov/vcf/vcftools/sara_sapho.lmiss ./
+scp -r -J nromerov@168.176.34.122 nromerov@perseus:/scratchsan/C_computacion/nromerov/vcf/vcftools/sara_sapho.het ./
