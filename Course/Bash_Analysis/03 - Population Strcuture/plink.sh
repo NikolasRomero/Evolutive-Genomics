dc9e24f4f2b9b2ff4fa8a4d8fb4f5715e4989817
Nicolás Romero <nromerov@unal.edu.co>
2026-07-29 13:52:26 -0500

    Create plink.sh

---
+++ Course/Bash_Analysis/03 - Population Strcuture/plink.sh
@@ -0,0 +1,51 @@
+
+cd ~/popStructure
+
+module load envs/anaconda3
+
+VCF="../vcf_real/sara_sapho_filtered.vcf.gz"
+
+
+module load apps/plink/2.0.0-a
+
+## Include bi-allelic sites only (excluding singletons), apply a minor allele filter of 0.05 and prune sites to 1 per 10 kb.
+
+plink2 \
+    --vcf $VCF \
+    --threads 8 \
+    --allow-extra-chr \
+    --geno 0.25 \
+    --min-alleles 2 \
+    --max-alleles 2 \
+    --maf 0.05 \
+    --bp-space 10000 \
+    --export vcf id-paste=iid \
+    --out sara_sapho_filtered_maf0.05_pruned
+
+# Update the VCF variable to the pruned one:
+
+VCF=sara_sapho_filtered_maf0.05_pruned
+
+# generate allele frequency file
+plink2 \
+    --vcf ${VCF}.vcf \
+    --threads 8 \
+    --allow-extra-chr \
+    --set-missing-var-ids @:# \
+    --freq \
+    --out ${VCF}
+
+# create pca
+plink2 \
+    --vcf ${VCF}.vcf \
+    --threads 8 \
+    --allow-extra-chr \
+    --set-missing-var-ids @:# \
+    --read-freq ${VCF}.afreq \
+    --pca \
+    --out ${VCF}
+
+
+
+
+
